deb8a44c71b04e6db1a0fd0c120c0f1c8a184a67
xy94k <upyakoman@gmail.com>
2023-03-04 14:11:59 +0500

    Update bot.py
    Хранение данных пользователя в файле json.

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import openai, re, logging, conf, os
+import openai, re, logging, conf, os, json
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
 from transformers import GPT2TokenizerFast
@@ -25,10 +25,35 @@ logging.basicConfig(level=logging.INFO)
 prompt = ""
 prompt_tokens = []
 
+async def get_user_data(user_id):
+    try:
+        with open(f"user_data/{user_id}.json") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return None
+
+async def save_user_data(user_id, user_data):
+    with open(f"user_data/{user_id}.json", "w") as f:
+        json.dump(user_data, f)
+        
+
 # Обработка команды /start
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.answer("Привет! Я бот на основе ChatGPT.")
+    user_id = message.from_user.id
+    user_data = await get_user_data(user_id)
+    if user_data is None:
+        await save_user_data(user_id, {
+        'model_engine' : "text-davinci-003",
+        'temperature' : 0.3,
+        'max_tokens' : 2047,
+        'top_p' : 0.2,
+        'frequency_penalty' : 0.2,
+        'presence_penalty' : 0.2,
+        'prompt' : "",
+        'base' : ""
+    })
 
 # Обработка команды /context
 @dp.message_handler(commands=['context'])
@@ -68,32 +93,32 @@ async def set_temperature(message: types.Message):
 @dp.message_handler()
 async def any_message(message: types.Message):
     # Получение текста сообщения от пользователя
-    global prompt
-    global prompt_tokens
+    user_id = message.from_user.id
+    user_data = await get_user_data(user_id)
     user_input = message.text
 
     # получить список токенов из ввода
     input_tokens = tokenizer.tokenize(user_input)
-    prompt_tokens = tokenizer.tokenize(prompt)
+    prompt_tokens = tokenizer.tokenize(user_data['prompt'])
     # вычислить общее количество токенов
     total_tokens = len(input_tokens) + len(prompt_tokens)
 
     # вычислить количество лишних токенов
-    excess_tokens = max(0, total_tokens - conf.max_prompt_tokens)
+    excess_tokens = max(0, total_tokens - (4097 - user_data['max_tokens']))
 
     # добавить ввод к истории, удаляя необходимое количество токенов
     if excess_tokens > 0:
         prompt_tokens = prompt_tokens[excess_tokens:]
-        prompt = tokenizer.convert_tokens_to_string(prompt_tokens) + " " + user_input
+        user_data['prompt'] = tokenizer.convert_tokens_to_string(prompt_tokens) + " " + user_input
     else:
-        prompt += user_input
+        user_data['prompt'] += user_input
 
     # Генерация ответа на основе текста сообщения
     try:
         response = openai.Completion.create(
-        engine=conf.model_engine,
-        prompt=prompt,
-        max_tokens=conf.max_tokens
+        engine=user_data['engine'],
+        prompt=user_data['prompt'],
+        max_tokens=user_data['max_tokens']
         )
 
         # Получение ответа из сгенерированного текста
@@ -101,22 +126,23 @@ async def any_message(message: types.Message):
 
         # получить список токенов из ответа
         response_tokens = tokenizer.tokenize(answer)
-        prompt_tokens = tokenizer.tokenize(prompt)
+        prompt_tokens = tokenizer.tokenize(user_data['prompt'])
 
         # вычислить общее количество токенов
         total_tokens = len(response_tokens) + len(prompt_tokens)
 
         # вычислить количество лишних токенов
-        excess_tokens = max(0, total_tokens - conf.max_prompt_tokens)
+        excess_tokens = max(0, total_tokens - user_data['max_tokens'])
 
         # добавить ответ к истории, удаляя необходимое количество токенов
         if excess_tokens > 0:
             prompt_tokens = prompt_tokens[excess_tokens:]
-            prompt = tokenizer.convert_tokens_to_string(prompt_tokens) + " " + answer
+            user_data['prompt'] = tokenizer.convert_tokens_to_string(prompt_tokens) + " " + answer
         else:
-            prompt += answer
+            user_data['prompt'] += answer
         # Отправка ответа пользователю
         await message.answer(answer)
+        await save_user_data(user_id,user_data)
     except openai.error.RateLimitError as e:
         await message.answer('Превышен лимит запросов:',e)
 
