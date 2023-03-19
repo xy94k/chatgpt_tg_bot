a70b1db4627f8170d9babe4df339c9dd6cce372f
xy94k <upyakoman@gmail.com>
2023-03-19 16:18:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -115,12 +115,12 @@ def num_tokens(messages):
     print(messages)
     for message_dict in messages:
         print(message_dict)
-        num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+        '''num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
         for key, value in message_dict.items():
             num_tokens += len(encoding.encode(value))
             if key == "name":  # if there's a name, the role is omitted
                 num_tokens += -1  # role is always required and always 1 token
-    num_tokens += 2  # every reply is primed with <im_start>assistant
+    num_tokens += 2  # every reply is primed with <im_start>assistant   '''
     return num_tokens
 
 # Обновить messages
